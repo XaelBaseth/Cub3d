544db5c5ee6e531f90cf4224313272ac35e1487c
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2023-10-16 08:20:05 +0200

    Cleaned up a bit and added minicub3d

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -1,8 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acharlot <acharlot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 08:19:10 by acharlot          #+#    #+#             */
+/*   Updated: 2023/10/16 08:19:12 by acharlot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CUB3D_H
 # define CUB3D_H
 
+/*-----------------------------------------------------------------------------
+								STRUCTURES
+-----------------------------------------------------------------------------*/
+
+# include "../lib/libft/inc/ft_gc_alloc.h"
+# include "../lib/libft/inc/ft_printf.h"
+# include "../lib/libft/inc/libft.h"
+# include "../lib/libft/inc/get_next_line.h"
 # include "../lib/minilibx/mlx.h"
 # include "X11/X.h"
 # include "X11/keysym.h"
+
+# include <math.h>
+
+
+/*-----------------------------------------------------------------------------
+								STRUCTURES
+-----------------------------------------------------------------------------*/
+
+typedef struct s_data	t_data;
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*mlx_win;
+}			t_data;
 
 #endif
--- src/main/miniCub3d.c
+++ src/main/miniCub3d.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   miniCub3d.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cpothin <cpothin@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/12 11:45:05 by cpothin           #+#    #+#             */
-/*   Updated: 2023/10/12 14:27:54 by cpothin          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../../inc/cub3d.h"
 
 #define mapWidth 24
@@ -17,81 +5,147 @@
 #define screenWidth 640
 #define screenHeight 480
 
-typedef struct	s_textures
+int worldMap[mapWidth][mapHeight]=
 {
-	void	*north;
-	void	*south;
-	void	*east;
-	void	*west;
-}				t_textures;
-
-typedef struct	s_color
-{
-	int	r;
-	int	g;
-	int	b;
-}				t_color;
-
-typedef struct	s_pos
-{
-	int		pos_x;
-	int		pos_y;
-	double	sub_x;
-	double	sub_y;
-	double	rotation;
-}				t_pos;
-
-typedef struct	data 
-{
-	void		*mlx;
-	void		*win;
-	t_textures	*textures;
-	char		**level;
-}				t_data;
-
-int worldMap[mapWidth][mapHeight] = 
-{
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-	{1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
+  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 };
 
-int main(int argc, char **argv)
+void drawVerticalLine(void *mlx, void *win, int x, int startY, int endY, unsigned int color)
 {
-	t_data data;
-	double posX = 22; //x and y start position
-	double posY = 12;
+    for (int y = startY; y <= endY; y++)
+        mlx_pixel_put(mlx, win, x, y, color);
+}
 
-	double dirX = -1; //initial direction vector
-	double dirY = 0;
-
-	double planeX = 0; //the 2d raycaster version of camera plane
-	double planeY = 0.66;
+int main()
+{
+	double posX = 22, posY = 12;  //x and y start position
+	double dirX = -1, dirY = 0; //initial direction vector
+	double planeX = 0, planeY = 0.66; //the 2d raycaster version of camera plane
 
 	double time = 0; //time of current frame
 	double oldTime = 0; //time of previous frame
+	t_data data;
 
 	data.mlx = mlx_init();
-	data.win = mlx_new_window(data.mlx, screenWidth, screenWidth, "Raycaster");
+	data.mlx_win = mlx_new_window(data.mlx, screenHeight, screenWidth, "Cub3d"); //We create the window
+
+	for (int x = 0; x < screenWidth; x++)
+	{
+		double cameraX = 2 * x / (double)screenWidth - 1; //x-coordinate in camera space
+		double rayDirX = dirX + planeX * cameraX;
+		double rayDirY = dirY + planeY * cameraX;
+		//which box of the map we're in
+		int mapX = (int)posX;
+		int mapY = (int)posY;
+		//lenght of ray from current position to next x or y side
+		double sideDistX;
+		double sideDistY;
+		//len of ray from one x or y side to next x or y side
+		double deltaDistX = (rayDirX == 0) ? 1e30 : abs(1 / rayDirX);
+		double deltaDistY = (rayDirY == 0) ? 1e30 : abs(1 / rayDirY);
+		double perpWallDist;
+		//what direction to step in x or y direction (either +1 or -1)
+		int stepX;
+		int stepY;
+
+		int hit = 0; //was there a wall hit?
+		int side; //was a North-South or a East-West wall hit?
+	      //calculate step and initial sideDist
+		if(rayDirX < 0)
+		{
+			stepX = -1;
+			sideDistX = (posX - mapX) * deltaDistX;
+		}
+		else
+		{
+			stepX = 1;
+			sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+		}
+		if(rayDirY < 0)
+		{
+			stepY = -1;
+			sideDistY = (posY - mapY) * deltaDistY;
+		}
+		else
+		{
+			stepY = 1;
+			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+      	}
+		//perform DDA
+		while (hit == 0)
+		{
+			//jump to next map squate, eithr in x-dir or in y-dir
+			if (sideDistX < sideDistY)
+			{
+				sideDistX += deltaDistX;
+				mapX += stepX;
+				side = 0;
+			}
+			else
+			{
+				sideDistY += deltaDistY;
+				mapY += stepY;
+				side = 1;
+			}
+			//Check if ray has hit a wall
+			if (worldMap[mapX][mapY] > 0)
+				hit = 1;
+		}
+		//Calculate distance projected on camera direction
+		if (side == 0)
+			perpWallDist = (sideDistX - deltaDistX);
+		else
+			perpWallDist = (sideDistY - deltaDistY);
+		//calculate height of the line to draw on screen
+		int lineHeight = (int)(screenHeight / perpWallDist);
+		//caculate the lowest and highest pixel to fill in the current stripe
+		int drawStart = -lineHeight / 2 + screenHeight / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		int drawEnd = lineHeight / 2 + screenHeight / 2;
+		if (drawEnd >= screenHeight)
+			drawEnd = screenHeight - 1;
+		// We trace the different colors according to wich side of the walls we are facing.
+		unsigned int color1 = mlx_get_color_value(data.mlx, 0xFF0000); // Red
+		unsigned int color2 = mlx_get_color_value(data.mlx, 0x00FF00); // Green
+		unsigned int color3 = mlx_get_color_value(data.mlx, 0x0000FF); // Blue
+		unsigned int color;
+		switch (side)
+		{
+			case 0:
+				color = color1;
+				break;
+			case 1:
+				color = color2;
+				break;
+			case 2:
+				color = color3;
+				break;
+		}
+		drawVerticalLine(data.mlx, data.mlx_win, x, drawStart, drawEnd, color);
+	}
+	mlx_loop(data.mlx); //We start the loop
 }
